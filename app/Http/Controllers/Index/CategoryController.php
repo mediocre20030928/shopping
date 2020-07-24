ddbb2fb4823dcd84c845491ece6f72d252278248
zjj <1621755657@qq.com>
2020-07-24 09:40:57 +0800

    123

--- app/Http/Controllers/Index/CategoryController.php
+++ app/Http/Controllers/Index/CategoryController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers\Index;
 
 use App\Http\Controllers\Controller;
 use App\model\CategoryModel;
+use App\model\GoodsModel;
+use App\model\TestModel;
 use Illuminate\Http\Request;
 
-use DB;
 class CategoryController extends Controller
 {
 
     public function category(){
        $cate_data=CategoryModel::where('parent_id',0)->get();
        $cate_all_data= $this->fl($cate_data->toArray());
-//        echo "<pre>";print_r($cate_all_data);echo "</pre>";die;
-       return view('shop.category',['cate_all_data'=>$cate_all_data]);
+
+        $get=TestModel::where('is_new',1)->orderBy( 'goods_id', 'DESC' )->limit(4)->get();//热卖
+        $pwa=TestModel::where('is_best',2)->orderBy('goods_id','desc')->limit(4)->get();//最新
+       return view('shop.category',['cate_all_data'=>$cate_all_data,'get'=>$get,'pwa'=>$pwa]);
     }
     public function fl($data,$pid=0,$le=0){
 //创建一个静态数组保存数据
@@ -28,7 +31,7 @@ class CategoryController extends Controller
 //将有关数据保存如数据
                 $array[]=$v;
 //为了将有关数据保存数据，这里使用递归
-                self::fl($data,$v['cat_id'],$le+1);
+                self::fl($data,$v['type_id'],$le+1);
 
             }
         }
@@ -36,7 +39,11 @@ class CategoryController extends Controller
         return $array;
     }
 
-
+//   public function test3(){
+//      $cate = CategoryModel::leftjoin('p_goods','p_goods.goods_id','=','p_category.cat_id')->get();
+//
+//       return view('shop.category',['list'=>$get]);
+//   }
 
 
 
